ba25d6140678824d1325123eb9ec3801f59c08d4
Juan Norris <juann9@gmail.com>
2013-12-16 17:26:46 -0300

    Fixed the context processor to set the ADMIN_MEDIA_PREFIX. PEP8 fixes

--- tekextensions/context_processors.py
+++ tekextensions/context_processors.py
@@ -1,18 +1,26 @@
 from django.conf import settings
 from django.contrib.sites.models import Site, RequestSite
 
+
 def admin_media_prefix(request):
-    return {'ADMIN_MEDIA_PREFIX': settings.ADMIN_MEDIA_PREFIX }
+    """
+    A context processor to add the ADMIN_MEDIA_PREFIX to the current Context.
+
+    Starting from django 1.4, the static files belonging to django admin follow
+    the standard conventions.
+    """
+    return {'ADMIN_MEDIA_PREFIX': settings.STATIC_URL + 'admin/'}
+
 
 def current_site(request):
-    '''
+    """
     A context processor to add the "current_site" to the current Context
-    '''
+    """
     context_name = 'CURRENT_SITE'
 
     try:
         current_site = Site.objects.get_current()
-        return { context_name: current_site, }
+        return {context_name: current_site, }
     except Site.DoesNotExist:
         # always return a dict, no matter what!
-        return { context_name: RequestSite(request)}
+        return {context_name: RequestSite(request)}
