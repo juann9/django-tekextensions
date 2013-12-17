e1e2f829a1ddc5f76a45f877bff476d06483704d
Juan Norris <juann9@gmail.com>
2013-12-17 13:35:11 -0300

    Minor fixes.

--- tekextensions/views.py
+++ tekextensions/views.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.decorators import login_required
+from django.forms import ValidationError
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.http import HttpResponse
 from django.utils.html import escape
-from tekextensions.forms import get_model_form, normalize_model_name
 
+from tekextensions.forms import get_model_form
+from tekextensions.utils import normalize_model_name
+
+
+@login_required
 def add_new_model(request, model_name, form=None):
     normal_model_name = normalize_model_name(model_name)
 
@@ -16,16 +21,20 @@ def add_new_model(request, model_name, form=None):
         if form.is_valid():
             try:
                 new_obj = form.save()
-            except forms.ValidationError, error:
+            except ValidationError:
                 new_obj = None
 
             if new_obj:
-                return HttpResponse('<script type="text/javascript">opener.dismissAddAnotherPopup(window, "%s", "%s");</script>' % \
-                    (escape(new_obj._get_pk_val()), escape(new_obj)))
+                script = ('<script type="text/javascript">'
+                          'opener.dismissAddAnotherPopup(window, "%s", "%s");'
+                          '</script>')
+                content = script % (escape(new_obj._get_pk_val()),
+                                    escape(new_obj))
+                return HttpResponse(content)
 
     else:
         form = form()
 
-    page_context = {'form': form, 'field': normal_model_name}
-    return render_to_response('popup.html', page_context, context_instance=RequestContext(request))
-
+    context = {'form': form, 'field': normal_model_name}
+    return render_to_response('popup.html', context,
+                              context_instance=RequestContext(request))
